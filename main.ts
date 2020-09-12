6893493be6ba23b2c1eb5c1f6174e38abb72e5e6
tomi34 <66636695+tomi34@users.noreply.github.com>
2020-09-12 15:40:03 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+function omár2 () {
+    if (734 < input.magneticForce(Dimension.Strength)) {
+    	
+    }
+}
+basic.forever(function () {
+    if (pins.analogReadPin(AnalogPin.P0) < 341) {
+    	
+    } else if (pins.analogReadPin(AnalogPin.P0) > 682) {
+    	
+    } else {
+    	
+    }
+})
