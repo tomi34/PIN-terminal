b21de96a0b0efbd733065f4ba65e16f8a9cf15eb
tomi34 <66636695+tomi34@users.noreply.github.com>
2020-09-13 09:47:47 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,82 @@
-function omár2 () {
-    if (734 < input.magneticForce(Dimension.Strength)) {
-    	
+let omárv = 0
+let omár = 0
+let kalibrltrtk = 0
+let PINb = 0
+let PIN = ""
+input.onPinPressed(TouchPin.P0, function () {
+    if (0 == omárv) {
+        if (pins.analogReadPin(AnalogPin.P1) < 341) {
+            omr()
+        } else if (pins.analogReadPin(AnalogPin.P1) > 682) {
+            omr()
+        } else if (pins.analogReadPin(AnalogPin.P1) < 682 && pins.analogReadPin(AnalogPin.P1) > 341) {
+            basic.showNumber(omár)
+        }
+    }
+})
+function omr () {
+    omárv = 0
+    basic.pause(5000)
+    while (!(input.pinIsPressed(TouchPin.P0))) {
+        if (input.buttonIsPressed(Button.A)) {
+            if (omárv < 10) {
+                omárv += 1
+            } else if (omárv < 100) {
+                omárv += 10
+            } else if (omárv < 1000) {
+                omárv += 100
+            }
+            basic.showNumber(omárv)
+            basic.pause(100)
+            basic.clearScreen()
+        } else if (input.buttonIsPressed(Button.B)) {
+            if (omárv < 11) {
+                omárv += -1
+            } else if (omárv < 101) {
+                omárv += -10
+            } else if (omárv < 1001) {
+                omárv += -100
+            }
+            basic.showNumber(omárv)
+            basic.pause(100)
+            basic.clearScreen()
+        }
+    }
+    fizets()
+}
+function fizets () {
+    while (!(pins.analogReadPin(AnalogPin.P2) < kalibrltrtk - 10)) {
+        basic.pause(100)
+    }
+    for (let index = 0; index < 4; index++) {
+        while (!(input.pinIsPressed(TouchPin.P0))) {
+            if (input.buttonIsPressed(Button.A)) {
+                PINb += 1
+            } else if (input.buttonIsPressed(Button.B)) {
+                PINb += -1
+            }
+        }
+        PIN = "" + PIN + PINb
+    }
+    if (PIN.includes("3794")) {
+        if (pins.analogReadPin(AnalogPin.P1) < 341) {
+            omár += omárv - omárv * 2
+        } else if (pins.analogReadPin(AnalogPin.P1) > 682) {
+            omár += omárv
+        }
     }
 }
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P0) < 341) {
-    	
-    } else if (pins.analogReadPin(AnalogPin.P0) > 682) {
-    	
-    } else {
-    	
+    if (pins.analogReadPin(AnalogPin.P2) > 1020) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+        kalibrltrtk = pins.analogReadPin(AnalogPin.P2)
     }
 })
